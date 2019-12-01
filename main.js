97ca4b05593f431fa4eb5aee8921bf56edadc8e4
Abigail Mitchell <abby.w.mitchell@gmail.com>
2019-12-01 16:59:23 -0700

    working next/previous buttons

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 $( document ).ready(() => {
 
+// DATA CONTROLLER
 const dataController = (() => {
 
   class Flashcard {
@@ -8,83 +9,75 @@ const dataController = (() => {
       this.question = question;
       this.answer = answer;
     }
-  }
+  };
 
-  return {
-    data: {
+  let data = {
       allCards: {
         cards: [
-          {question: "What are ES6 variable names?", answer: "'let' and 'const'"},
-          {question: "Can the value of a 'const' variable be reassigned?", answer: "No"},
-          {question: "How do you write an arrow function?", answer: "const myFunction = () => { #code in here }"},
-          {question: "What are rest params?", answer: "Rest parameters allow you to accept an infinite number of arguments"},
-          {question: "What does string interpolation use in ES6?", answer: "Backticks"},
-          {question: "What are spread opperators?", answer: "Spread operators are similar to rest parameters except they are used to call a function instead of in the function signature."},
-          {question: "What is Object Destructing?", answer: "With the destructuring syntax, you can extract smaller fragments from arrays and objects"},
-          {question: "What is a Module?", answer: "A module is nothing more than a chunk of JavaScript code written in a file"},
-          {question: "What are ES6 Classes?", answer: "ES6 Classes employ inheritance hierarchies using functions and prototypes"},
-          {question: "When did ES6 come out?", answer: "2015"}
+          {id: 1, question: "What are ES6 variable names?", answer: "'let' and 'const'"},
+          {id: 2, question: "Can the value of a 'const' variable be reassigned?", answer: "No"},
+          {id: 3, question: "How do you write an arrow function?", answer: "const myFunction = () => { #code in here }"},
+          {id: 4, question: "What are rest params?", answer: "Rest parameters allow you to accept an infinite number of arguments"},
+          {id: 5, question: "What does string interpolation use in ES6?", answer: "Backticks"},
+          {id: 6, question: "What are spread opperators?", answer: "Spread operators are similar to rest parameters except they are used to call a function instead of in the function signature."},
+          {id: 7, question: "What is Object Destructing?", answer: "With the destructuring syntax, you can extract smaller fragments from arrays and objects"},
+          {id: 8, question: "What is a Module?", answer: "A module is nothing more than a chunk of JavaScript code written in a file"},
+          {id: 9, question: "What are ES6 Classes?", answer: "ES6 Classes employ inheritance hierarchies using functions and prototypes"},
+          {id: 10, question: "When did ES6 come out?", answer: "2015"}
         ]
-      }
-    },
-    addCard: (ques, ans) => {
-      let newCard;
+      } 
+    };
 
-      if (data.allCards['cards'].length > 0) {
-        ID = data.allCards['cards'][data.allCards['cards'].length-1].id + 1;
+  return {
+    addCard: (ques, ans) => {
+      let newCard, theData;
+      theData = data.allCards['cards'];
+
+      if (theData.length > 0) {
+        ID = theData[theData.length-1].id + 1;
       } else {
         ID = 0;
       }
 
       newCard = new Flashcard(ID, ques, ans);
       //this is using bracket notation to access the object value, not accessing array
-      data.allCards['cards'].push(newCard);
-      return newCard;
+      theData.push(newCard);
+      return newCard
+    }, 
+    allData: () => {
+      let theData = data.allCards['cards'];
+      let currentC = data.allCards['currentCard'];
       
-    }
+      return {theData, currentC}
+    }    
   }
-  
-
 })();
 
-const uiController = (() => {
-
+//UI CONTROLLER
+const inputController = (() => {
+  
   return {
     getInput: () => {
       return {
         question: document.getElementById('q-input').value,
         answer:document.getElementById('a-input').value
       };
-    },
-    addNewCard: (obj) => {
-      let htmlQ, htmlA, elementQ, elementA;
-      
-      elementQ = 'content-q';
-      htmlQ = '<h3 id="question-info">%question%</h3>';
-    
-      elementA = 'content-a';
-      htmlA = '<h4 id="answer-info">%answer%</h4>';
-      
-      newHtmlQ = htmlQ.replace('%question%', obj.question);
-      newHtmlA = htmlA.replace('%answer%', obj.answer);
-
-      document.getElementById(elementQ).insertAdjacentHTML('beforeend', newHtmlQ);
-      document.getElementById(elementA).insertAdjacentHTML('beforeend', newHtmlA);
-      $('.form').toggle();
-      
     }
   }
-
 })();
 
-const controller = ((dataCtrl, uiCtrl) => {
+// MAIN CONTROLLER
+const controller = ((dataCtrl, inputCtrl) => {
+  let currentCard = 0;
 
   const setUpEventListeners = () => {
-
-    document.querySelector("flashcard").addEventListener("click", toggleCard);
-
+  
+    document.getElementById("flashcard").addEventListener("click", switchCard);
+    document.getElementById("next").addEventListener("click", next);
+    document.getElementById("previous").addEventListener("click", previous);
+    document.getElementById("edit").addEventListener("click", edit);
+    document.getElementById("deleteCard").addEventListener("click", deleteCard);
     document.getElementById("new-flashcard").addEventListener("click", toggleForm);
-
     document.getElementById("create").addEventListener("click", ctrlAddCard);
 
     document.addEventListener("keypress", (event) => {
@@ -94,17 +87,13 @@ const controller = ((dataCtrl, uiCtrl) => {
     });
   };
 
-  const toggleCard = () => {
-    
-  }
+  const displayFirstCard = (currentCard) => {
+    let cards, elementQ, elementA, htmlQ, htmlA, allData;
+    // currentCard = current;
 
-  const toggleForm = () => {
-    $(".form").toggle();
-  }
-  const displayFirstCard = () => {
-    let cards, currentCard, elementQ, elementA, htmlQ, htmlA;
-    cards = dataCtrl.data.allCards['cards'];
-    currentCard = 0;
+    allData = dataCtrl.allData().theData;
+
+    cards = allData;
     elementQ = 'content-q';
     htmlQ = '<h3 id="question-info">%question%</h3>';
   
@@ -118,31 +107,81 @@ const controller = ((dataCtrl, uiCtrl) => {
       document.getElementById(elementQ).insertAdjacentHTML('beforeend', newHtmlQ);
       document.getElementById(elementA).insertAdjacentHTML('beforeend', newHtmlA);
 
-
-
-      $("#question-info").show();
     } else if (cards.length <= 0) {
       newHtmlQ = htmlQ.replace('%question%', "No cards");
+      document.getElementById(elementQ).insertAdjacentHTML('beforeend', newHtmlQ);
     }
   };
 
-  const ctrlAddCard = () => {
-    let input, newCard; 
+  const next = () => {
+    let cards;
+    allData = dataCtrl.allData().theData;
+    cards = allData;
 
-    input = uiCtrl.getInput();
-    newCard = dataCtrl.addCard(input.question, input.answer);
-    uiCtrl.addNewCard(newCard);
+    if (currentCard < (cards.length - 1)) {
+      currentCard ++ 
+    } else {
+      currentCard = 0
+    }  
+    switchCard();
+  };
+
+  const previous = () => {
+    let cards;
+    allData = dataCtrl.allData().theData;
+    cards = allData;
+
+    if (currentCard > 0) {
+      currentCard --;
+    } else {
+      currentCard = (cards.length - 1)
+    }
+    switchCard();
+  };
+ 
+   const switchCard = () => {
+    let cards, newElementQ, newElementA, cardQuestion, cardAnswer;
+    
+    allData = dataCtrl.allData().theData;
+    cards = allData;
+
+    cardQuestion = cards[currentCard].question;
+    newElementQ = document.getElementById('content-q');
+    newElementQ.innerHTML = cardQuestion;
+    $('#content-q').toggle();
+
+    cardAnswer = cards[currentCard].answer;
+    newElementA = document.getElementById('content-a');
+    newElementA.innerHTML = cardAnswer;
+    $('#content-a').toggle();
+
+    };
+
+  const toggleForm = () => {
+    $("#content-q").hide();
+    $("#content-a").hide();
+    $(".form").toggle();
+  }
+
+  const ctrlAddCard = () => {
+    let input; 
+
+    input = inputCtrl.getInput();
+    
+    $('.form').toggle();
+    $("#content-q").show();
+    dataCtrl.addCard(input.question, input.answer);
+    
   };
 
   return {
     init: () => {
-      console.log("App has started");
-      displayFirstCard();
+      displayFirstCard(currentCard);
       setUpEventListeners();
     }
   }
 
-})(dataController, uiController);
+})(dataController, inputController);
 
 controller.init();
 
